c9e8de76a933ed0dfff954b0f5bc5996f42f2ebc
bobagby78 <bobagby78@gmail.com>
2021-07-16 18:47:55 -0500

    created many files today.

--- src/main/java/org/launchcode/dndproject/models/BaseCharacter.java
+++ src/main/java/org/launchcode/dndproject/models/BaseCharacter.java
@@ -1,4 +1,8 @@
+package org.launchcode.dndproject.models;
+
 public class BaseCharacter {
+    private String name;
+
 
     /*TODO make the base character class an abstract class for all the rest. start by building each subclass.
      What fields does it need in the abstract class and what methods can we make to put into an interface?
