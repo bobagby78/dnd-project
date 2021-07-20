6e1ef708f7a170b9bd235abe6e53e94085eb6982
bobagby78 <bobagby78@gmail.com>
2021-07-20 14:24:12 -0500

    basic structure completed for the project. running into some errors. will try from intellij community to see if any differences

---
+++ src/main/java/org/launchcode/dndproject/homeController.java
@@ -0,0 +1,17 @@
+package org.launchcode.dndproject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class homeController {
+
+    @GetMapping("index")
+    public String index (Model model){
+        model.addAttribute("title", "The Dungeon");
+
+        return "index";
+    }
+
+}
