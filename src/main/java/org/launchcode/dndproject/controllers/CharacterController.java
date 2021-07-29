ceff6c62524e69efeb767f94f0613e68f62b4e06
bobagby78 <bobagby78@gmail.com>
2021-07-29 08:20:14 -0500

    BaseCharacter class has constructor, getters, and setters CharacterController working for GetMapping on index and create views create renders form with dropdowns and text inputs. need to finish 'id' and 'name' values for each input. fragments links fixed. identity deleted index and character/index views working beautifully with internal and external links. view.html needs some work.

--- src/main/java/org/launchcode/dndproject/controllers/CharacterController.java
+++ src/main/java/org/launchcode/dndproject/controllers/CharacterController.java
@@ -1,17 +1,32 @@
-//package org.launchcode.dndproject.controllers;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//@RequestMapping("character")
-//public class CharacterController {
-//
-//
-//    @GetMapping("create")
-//    public String renderCreateCharacterForm(){
-//        return "character/create";
-//    }
-//
-//}
+package org.launchcode.dndproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("character")
+public class CharacterController {
+
+    @GetMapping
+    public String characterIndex(){
+        return "character/index";
+    }
+
+    @GetMapping("view/{characterId}")
+    public String displayCharacterById(){
+        return "view/{characterId}";
+    }
+
+    @GetMapping("create")
+    public String renderCreateCharacterForm(){
+        return "character/create";
+    }
+
+    @PostMapping("create")
+    public String processCreateCharacterForm(){
+        return "redirect:";
+    }
+
+}
--- src/main/java/org/launchcode/dndproject/models/BaseCharacter.java
+++ src/main/java/org/launchcode/dndproject/models/BaseCharacter.java
@@ -3,17 +3,101 @@ package org.launchcode.dndproject.models;
 import javax.persistence.GeneratedValue;
 import javax.validation.Valid;
 import javax.validation.constraints.Size;
+import java.util.Objects;
 
 public class BaseCharacter {
-    //@GeneratedValue
+
+    @GeneratedValue
     private Integer id;
 
     @Size(min=3 , max= 250, message = "Please enter a character name between 3 and 250 characters" )
     private String name;
+    private String charClass;
+    private String background;
+    private String race;
+    private String alignment;
+    private String xp;
 
+    public BaseCharacter() {}
 
-    /*TODO make the base character class an abstract class for all the rest. start by building each subclass.
-     What fields does it need in the abstract class and what methods can we make to put into an interface?
-     */
+    public BaseCharacter(Integer id, String name, String charClass, String background, String race, String alignment, String xp) {
+        this.id = id;
+        this.name = name;
+        this.charClass = charClass;
+        this.background = background;
+        this.race = race;
+        this.alignment = alignment;
+        this.xp = xp;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCharClass() {
+        return charClass;
+    }
+
+    public void setCharClass(String charClass) {
+        this.charClass = charClass;
+    }
+
+    public String getBackground() {
+        return background;
+    }
+
+    public void setBackground(String background) {
+        this.background = background;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public String getAlignment() {
+        return alignment;
+    }
+
+    public void setAlignment(String alignment) {
+        this.alignment = alignment;
+    }
+
+    public String getXp() {
+        return xp;
+    }
+
+    public void setXp(String xp) {
+        this.xp = xp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseCharacter that = (BaseCharacter) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(charClass, that.charClass) && Objects.equals(background, that.background) && Objects.equals(race, that.race) && Objects.equals(alignment, that.alignment) && Objects.equals(xp, that.xp);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, charClass, background, race, alignment, xp);
+    }
+
 
 }
